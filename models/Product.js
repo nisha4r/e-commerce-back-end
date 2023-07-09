cf9912c2b2cb119d5f1a2870c25380d4ed1b18dc
nisha4r <nisha.4r@gmail.com>
2023-07-09 11:19:41 -0600

    add product column definition

--- models/Product.js
+++ models/Product.js
@@ -4,25 +4,43 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    id:{
-
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
-    product_name:{
-
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    price:{
-
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,      
+      Validate: {
+        isDecimal: true
+      }
     },
-    stock:{
-
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      Validate: {
+        isNumeric: true
+      }
     },
     category_id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      reference:{
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
