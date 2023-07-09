ad3a739ad1fc36bdb4da410a7843d2476a9820fc
nisha4r <nisha.4r@gmail.com>
2023-07-09 11:39:22 -0600

    findall category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,18 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+
+  }).then(db => {
+
+  })
+  res.json(db);
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  
 });
 
 router.post('/', (req, res) => {
