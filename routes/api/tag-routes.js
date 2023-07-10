0f6cbdd557e619d1dcba885f943d34c477a4013a
nisha4r <nisha.4r@gmail.com>
2023-07-09 20:24:26 -0600

    Tag findAll implementation

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,47 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  Tag.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  }).then(db => {
+    if (!db) {
+      res.status(404).json({ message: 'Not found any categories' });
+      return;
+    }
+    res.json(db);
+  }).catch(error => {
+    res.status(500).json(error);
+    console.log(error);
+  });
+
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+
+  }).then(data => {
+    if (!data) {
+      res.status(404).json({ message: 'Requested Category is not found' });
+      return;
+    }
+    res.json(data);
+  }).catch(error => {
+    res.status(500).json(error);
+    console.log(error);
+  });
+
 });
 
 router.post('/', (req, res) => {
