897e9ec8edd89ba73112aaec2c72d1fd1fcd13a8
nisha4r <nisha.4r@gmail.com>
2023-07-24 19:24:14 -0600

    update with comments on category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,19 +5,22 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  // includes product id, product_name, price, stock, category_id
   Category.findAll({
     include: {
       model: Product,
       attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   }).then(db => {
+    //when there is no category match found, return error 404 with message.
     if (!db) {
       res.status(404).json({ message: 'Not found any categories' });
       return;
     }
+    //successful call
     res.json(db);
   }).catch(error => {
+    //return error 500 if there is anyother error
     res.status(500).json(error);
     console.log(error);
   });
@@ -25,8 +28,8 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // find one category by product `id` value
+  // includes following vlaues in the repsonse: product id, product_name, price, stock, category_id
   Category.findOne({
     where: {
       id: req.params.id
@@ -37,12 +40,15 @@ router.get('/:id', (req, res) => {
     }
 
   }).then(data => {
+    //no match found in the requested category
     if (!data) {
       res.status(404).json({ message: 'Requested Category is not found' });
       return;
     }
+    //successful match
     res.json(data);
   }).catch(error => {
+    //for all other errors
     res.status(500).json(error);
     console.log(error);
   });
@@ -50,14 +56,16 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // create a new category with category name from post request body
   Category.create({
 
     category_name: req.body.category_name
 
   }).then(data => {
+    //success response with data
     res.json(data);
   }).catch(error => {
+    // for all other errors.
     res.status(500).json(error);
     console.log(error);
   });
@@ -65,18 +73,22 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // update a category by product `id` value
   Category.update(req.body, {
     where: {
       id: req.params.id
     }
   }).then(data => {
+    //No requested category found to update
     if (!data) {
       res.status(404).json({ message: 'Requested Category is not found' });
       return;
     }
+
+    //success response
     res.json(data);
   }).catch(error => {
+    // for all other errors
     res.status(500).json(error);
     console.log(error);
   });
@@ -84,18 +96,21 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  // delete a category by product `id` value
   Category.destroy({
     where: {
       id: req.params.id
     }
   }).then(data => {
+    //no requested category found
     if (!data) {
       res.status(404).json({ message: 'Requested Category is not found' });
       return;
     }
+    //successful response
     res.json(data);
   }).catch(error => {
+    // for all other  errors.
     res.status(500).json(error);
     console.log(error);
   });
