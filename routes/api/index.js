30a6f2f8406338d93c74e44d4743f08e211f7f58
nisha4r <nisha.4r@gmail.com>
2023-07-24 20:06:29 -0600

    comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,8 +4,8 @@ const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes); // categories api route
-router.use('/products', productRoutes);//  product api routes
-router.use('/tags', tagRoutes); //tag api routes
+router.use('/categories', categoryRoutes); // categories api routes
+router.use('/products', productRoutes);//  products api routes
+router.use('/tags', tagRoutes); //tagds api routes
 
 module.exports = router;
