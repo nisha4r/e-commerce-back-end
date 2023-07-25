b822dd46a8e0fcdb83d216e8371df62f404f17d0
nisha4r <nisha.4r@gmail.com>
2023-07-24 20:05:54 -0600

    api routes comments

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+// import all the routes 
 const categoryRoutes = require('./category-routes');
 const productRoutes = require('./product-routes');
 const tagRoutes = require('./tag-routes');
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use('/categories', categoryRoutes); // categories api route
+router.use('/products', productRoutes);//  product api routes
+router.use('/tags', tagRoutes); //tag api routes
 
 module.exports = router;
